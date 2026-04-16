e386f7cdf42b2f746034e13871cec6d41e4df1bc
p-ganesh45 <get.pganesh@gmail.com>
2026-04-16 07:19:43 +0530

    disable security and final fix

--- src/main/java/com/college/uniportal/UniportalApplication.java
+++ src/main/java/com/college/uniportal/UniportalApplication.java
@@ -3,15 +3,16 @@ package com.college.uniportal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-    org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
-    org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication(
+    exclude = {
+        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+    }
+)
 public class UniportalApplication {
 
-	public static void main(String[] args) {
-
-		SpringApplication.run(UniportalApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(UniportalApplication.class, args);
+    }
 }
