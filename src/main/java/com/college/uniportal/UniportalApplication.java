06d05d659752673eaeee784046e3e1889675e35a
p-ganesh45 <get.pganesh@gmail.com>
2026-04-16 06:50:11 +0530

    disable jpa for render

--- src/main/java/com/college/uniportal/UniportalApplication.java
+++ src/main/java/com/college/uniportal/UniportalApplication.java
@@ -3,7 +3,10 @@ package com.college.uniportal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
+})
 public class UniportalApplication {
 
 	public static void main(String[] args) {
